f7a07b61b5c8440bb1c23f1d173ff4814e8a1342
roland.sanou <66534739+rolandsanou@users.noreply.github.com>
2024-03-30 08:59:49 +0300

    useful update part 1

--- app/src/main/java/com/roland/urlblocker/services/ExampleService.java
+++ app/src/main/java/com/roland/urlblocker/services/ExampleService.java
@@ -2,8 +2,11 @@ package com.roland.urlblocker.services;
 
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -14,6 +17,27 @@ public class ExampleService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+//    public class NetworkConnection extends BroadcastReceiver {
+//        private Context _context;
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            boolean noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+//            this._context =context;
+//            if(!noConnectivity) {
+//                onConnectionFound();
+//            } else {
+//                onConnectionLost();
+//            }
+//        }
+//
+//        public void onConnectionLost() {
+//            PreferenceHelper.getInstance(_context).setIsInternetConnection(false);
+//        }
+//
+//        public void onConnectionFound() {
+//            PreferenceHelper.getInstance(_context).setIsInternetConnection(true);
+//        }
+//    }
 
     private void TimeSettingReceiverStart(){
         UrlScreen screen = new UrlScreen();
